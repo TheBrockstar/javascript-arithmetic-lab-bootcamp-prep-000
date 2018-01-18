71c9ca64c2586ec00bb62779d18d914406c0e64b
Brock Rohloff <00agentb@gmail.com>
2018-01-18 08:19:58 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,5 +4,5 @@ function multiple(){}
 function divide(){}
 function inc(n){}
 function dec(n){}
-function makeInt(n)
-function preserverDecimal(n){}learn
+function makeInt(n){}
+function preserverDecimal(n){}
