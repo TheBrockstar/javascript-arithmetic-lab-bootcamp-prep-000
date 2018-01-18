bc3877cdb9ecae613c435eefbb1af2b62c48f318
Brock Rohloff <00agentb@gmail.com>
2018-01-18 08:19:00 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,8 @@
+function add(){183 + 34}
+function subtract(){}
+function multiple(){}
+function divide(){}
+function inc(n){}
+function dec(n){}
+function makeInt(n)
+function preserverDecimal(n){}
