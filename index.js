8ca34ef026c5946875633c4da4b3c890f06d56f2
Brock Rohloff <00agentb@gmail.com>
2018-01-18 08:20:59 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 function add(){183 + 34}
-function subtract(){}
-function multiple(){}
-function divide(){}
+function subtract(197 - 15){}
+function multiply(21 * 2){}
+function divide(84 / 2){}
 function inc(n){}
 function dec(n){}
 function makeInt(n){}
