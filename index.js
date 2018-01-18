78a51134ddcf1050e6ad5a85eb1b1c57850ebfab
Brock Rohloff <00agentb@gmail.com>
2018-01-18 08:30:00 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -5,4 +5,4 @@ function divide(a,b){a /= b; return a;}
 function inc(n){n++; return n;}
 function dec(n){n--;return n;}
 function makeInt(n){n = parseInt(n, 10); return n;}
-function preserverDecimal(n){n = parseFloat(n); return n;}
+function preserveDecimal(n){n = parseFloat(n); return n;}
